835f127eed1602ae1a6993f954357e45c75256dd
vodolazov18 <andrei.ynwa@yandex.ru>
2025-01-30 14:21:20 +0300

    Initial commit

---
+++ ch_01/coffeeshop.py
@@ -0,0 +1,12 @@
+print("Welcome to Andrey's coffee shop.")
+
+name = input("Please enter your name: ")
+drink = input ("Please enter your drink: ")
+flavor = input("Please enter your flavor: ")
+topping = input("Please enter your topping: ")
+
+print(f"Here is your order, {name}")
+print(f"Main product: {drink}")
+print(f"Flavor: {flavor}")
+print(f"Topping: {topping}")
+print(f"Thanks for your order!")
